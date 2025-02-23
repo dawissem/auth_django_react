8ff5c5ac9ba44096a63b85aa367861cbf28649c0
dawissem <dawissem.wm@gmail.com>
2025-02-23 16:30:39 +0100

    first commit

---
+++ backend/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+# Create your models here.
+
+
+#class CustemUserManager(BaseUserManager):
+ #   def create_user():
+
+class customeUser(AbstractUser):
+    email = models.EmailField(max_length=200,unique=True)
+    birthday =models.DateField(null=True, blank=True)
+    USERNAME_FIELD='email'     
+    REQUIRED_FIELDS= []
