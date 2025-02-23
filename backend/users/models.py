76e5d0e4c13636f0b6ede5d4ae1342e28ae6a751
dawissem <dawissem.wm@gmail.com>
2025-02-23 17:02:23 +0100

    finalisation of model users + creating superuser

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,13 +5,29 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class CustemUserManager(BaseUserManager):
-   def create_user():
-       pass
-   def create_suepruser():
-       pass
+   def create_user(self,email,password=None ,**extra_fields):
+       if not email:
+           raise ValueError('Email is required')
+       if not email.endswith('@societex.com'):
+            raise ValueError('Email must end with @societex.com')
+       email = self.normalize_email(email)
+       user =self.model(email=email,**extra_fields)
+       user.set_password(password)
+       user.save(using=self._db)
+       
+       return user
+   
+   def create_superuser(self,email,password=None, **extra_fields):
+       extra_fields.setdefault('is_staff', True)
+       extra_fields.setdefault('is_superuser', True)
+       return self.create_user(email,password,**extra_fields)
 
 class customeUser(AbstractUser):
     email = models.EmailField(max_length=200,unique=True)
     birthday =models.DateField(null=True, blank=True)
+    username =models.EmailField(max_length=200,null=True, blank=True)
+    
+    objects = CustemUserManager()
+    
     USERNAME_FIELD='email'     
     REQUIRED_FIELDS= []
