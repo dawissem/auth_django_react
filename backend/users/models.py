4be8097d623da2a9ded2dfc911f619a883652978
dawissem <dawissem.wm@gmail.com>
2025-02-23 16:37:01 +0100

    pass superuse in backend (bypass)

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,8 +4,11 @@ from django.contrib.auth.base_user import BaseUserManager
 # Create your models here.
 
 
-#class CustemUserManager(BaseUserManager):
- #   def create_user():
+class CustemUserManager(BaseUserManager):
+   def create_user():
+       pass
+   def create_suepruser():
+       pass
 
 class customeUser(AbstractUser):
     email = models.EmailField(max_length=200,unique=True)
